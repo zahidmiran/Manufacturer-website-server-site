98e6ba6dbdeccea733d27cf6af09f5b3ca825afd
zahidmiran <zahidmiron599@gmail.com>
2022-05-27 10:36:03 +0600

    heroku is problems

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
-
+/* fasd */
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plyz6.mongodb.net/?retryWrites=true&w=majority`;
